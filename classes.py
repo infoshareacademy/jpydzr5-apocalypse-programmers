38dc8e80e54c71f0ee372197e84a792878684bba
Tomasz Brzezina <tomasz@brzezina.pl>
2024-05-28 23:52:36 +0200

    formatting PEP

--- classes.py
+++ classes.py
@@ -4,7 +4,15 @@ from datetime import datetime
 
 class Event:
     """Przodek klas związanych z wydarzeniem"""
-    def __init__(self, id: int, name: str, event_type: str, start_time:datetime, end_time: datetime, creator_email: str):
+    def __init__(
+            self,
+            id: int,
+            name: str,
+            event_type: str,
+            start_time:datetime,
+            end_time: datetime,
+            creator_email: str
+    ):
         super().__init__(self)
         self.id = id  # zamienić na generator identyfikatorow, zeby nie bylo duplikatow
         self.name = name  # unikalny indentyfikator wydarzenia
@@ -16,7 +24,12 @@ class Event:
 
 class Person:
     """Przodek klas związanych z osobami"""
-    def __init__(self, id: int, email: str, password: str):
+    def __init__(
+            self,
+            id: int,
+            email: str,
+            password: str
+    ):
         self.id = id  # zamienić na generator identyfikatorow, zeby nie bylo duplikatow
         self.email = email  # unikalny indentyfikator osoby
         self.password = password
@@ -32,7 +45,14 @@ class EventCreator(Person):
 
 class Ticket:
     """Pojedynczy bilet"""
-    def __init__(self, id: int, event_name: str, participant_email: str, row: str, place: str ):
+    def __init__(
+            self,
+            id: int,
+            event_name: str,
+            participant_email: str,
+            row: str,
+            place: str
+    ):
         super().__init__(self)
         self.id = id  # zamienić na generator identyfikatorow, zeby nie bylo duplikatow
         self.event_name = event_name  # relacja do wydarzenia
